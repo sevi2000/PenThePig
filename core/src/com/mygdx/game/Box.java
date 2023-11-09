67d796ee5561ba8109eb5d7e2c58061816041ce9
HydrolienF <hydrolien.f@gmail.com>
2023-11-09 14:36:57 +0100

    Reduce by 20 ram consumption

--- core/src/com/mygdx/game/Box.java
+++ core/src/com/mygdx/game/Box.java
@@ -12,86 +12,94 @@ import com.badlogic.gdx.utils.Align;
 import space.earlygrey.shapedrawer.ShapeDrawer;
 
 public class Box extends Actor {
-    boolean up,left,right,bottom;
-    Box upN,leftN,rightN,bottomN;
-    Box(float x, float y){
+    private static ShapeDrawer sd;
+    boolean up, left, right, bottom;
+    Box upN, leftN, rightN, bottomN;
+    Box(float x, float y) {
         up = false;
         left = false;
         right = false;
         bottom = false;
-        setSize(60,60);
-        addListener(new ClickListener(){
+        setSize(60, 60);
+        addListener(new ClickListener() {
             public void clicked(InputEvent event, float x, float y) {
                 System.out.println("clickedddd");
-                if(x > 10 && x < getWidth() - 10 && y > getHeight() - 10){
+                if (x > 10 && x < getWidth() - 10 && y > getHeight() - 10) {
                     System.out.println("up");
                     up = true;
                     if (upN != null)
                         upN.bottom = true;
                 }
 
-                else if(x > 10 && x < getWidth() - 10 && y < 10){
+                else if (x > 10 && x < getWidth() - 10 && y < 10) {
                     System.out.println("bottom");
                     bottom = true;
                     if (bottomN != null)
                         bottomN.up = true;
-                }
-                else if (x < 10 && y > 10 && y < getHeight() - 10) {
+                } else if (x < 10 && y > 10 && y < getHeight() - 10) {
                     System.out.println("left");
                     left = true;
                     if (leftN != null)
                         leftN.right = true;
-                }
-                else if (x > getWidth() - 10 && y > 10 && y < getHeight() - 10){
+                } else if (x > getWidth() - 10 && y > 10 && y < getHeight() - 10) {
                     right = true;
                     if (rightN != null)
                         rightN.left = true;
                 }
             }
         });
-        setPosition(x,y);
+        setPosition(x, y);
 
     }
     @Override
-    public void draw(Batch batch, float parentAlpha){
-        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
-        pixmap.setColor(Color.WHITE);
-        pixmap.drawPixel(0, 0);
-        Texture texture = new Texture(pixmap); // remember to dispose of later
-        pixmap.dispose();
-        TextureRegion region = new TextureRegion(texture, 0, 0, 1, 1);
-        ShapeDrawer sd = new ShapeDrawer(batch, region);
-        //sd.setColor(Color.BLACK);
-        //sd.filledRectangle(getX(),getY(),getWidth(),getHeight());
+    public void draw(Batch batch, float parentAlpha) {
+
+        sd = getShapeDrawer(batch);
+
+        // sd.setColor(Color.BLACK);
+        // sd.filledRectangle(getX(),getY(),getWidth(),getHeight());
         sd.setColor(Color.RED);
-        sd.filledRectangle(getX(),getY(),10,10);
-        sd.filledRectangle(getX()+50,getY(),10,10);
-        sd.filledRectangle(getX(),getY()+50,10,10);
-        sd.filledRectangle(getX()+50,getY()+50,10,10);
-        if (up){
+        sd.filledRectangle(getX(), getY(), 10, 10);
+        sd.filledRectangle(getX() + 50, getY(), 10, 10);
+        sd.filledRectangle(getX(), getY() + 50, 10, 10);
+        sd.filledRectangle(getX() + 50, getY() + 50, 10, 10);
+        if (up) {
             sd.setColor(Color.GREEN);
-            sd.filledRectangle(getX()+10,getY()+54,40,3);
+            sd.filledRectangle(getX() + 10, getY() + 54, 40, 3);
         }
-        if (left){
+        if (left) {
             sd.setColor(Color.RED);
-            sd.filledRectangle(getX()+3,getY()+10,3,40);
+            sd.filledRectangle(getX() + 3, getY() + 10, 3, 40);
         }
-        if (bottom){
+        if (bottom) {
             sd.setColor(Color.BLUE);
-            sd.filledRectangle(getX()+10,getY()+5,40,3);
+            sd.filledRectangle(getX() + 10, getY() + 5, 40, 3);
         }
-        if (right){
+        if (right) {
             sd.setColor(Color.YELLOW);
-            sd.filledRectangle(getX()+54,getY()+10,3,40);
+            sd.filledRectangle(getX() + 54, getY() + 10, 3, 40);
         }
-        if(penThePig()){
+        if (penThePig()) {
             sd.setColor(Color.PINK);
-            sd.circle(getX(Align.center),getY(Align.center),5);
+            sd.circle(getX(Align.center), getY(Align.center), 5);
         }
     }
     boolean penThePig() {
 
-         return up && bottom && left && right;
+        return up && bottom && left && right;
+    }
+
+    public ShapeDrawer getShapeDrawer(Batch batch) {
+        if (sd == null) {
+            Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+            pixmap.setColor(Color.WHITE);
+            pixmap.drawPixel(0, 0);
+            Texture texture = new Texture(pixmap); // remember to dispose of later
+            pixmap.dispose();
+            TextureRegion region = new TextureRegion(texture, 0, 0, 1, 1);
+            sd = new ShapeDrawer(batch, region);
+        }
+        return sd;
     }
 
 }
